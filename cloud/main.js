65cf6457be2c97523cee92ae071aa8649b6da177
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-16 12:26:10 -0700

    feat: add sign-in function

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,9 @@
-Parse.Cloud.define("hello", (request) => {
-	return "Hello world leo Silva!";
-});
+Parse.Cloud.define('v1-sign-in', async (req) => {
+	const user = await Parse.User.logIn(req.params.email.toLowerCase(), req.params.password)
+	return formatUser(user.toJSON())
+  }, {
+	fields: {
+	  email: { required: true },
+	  password: { required: true },
+	}
+  })
