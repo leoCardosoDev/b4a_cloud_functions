48c8e699ebc50ccb79a9dd4b10a2443ef8616a14
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-16 12:27:37 -0700

    feat: add sign-up function endpoint

--- cloud/main.js
+++ cloud/main.js
@@ -11,3 +11,24 @@ Parse.Cloud.define('v1-sign-in', async (req) => {
   Parse.Cloud.define('v1-get-user', async (req) => {
 	return req.user
   })
+
+  Parse.Cloud.define('v1-sign-up', async (req) => {
+	const user = new Parse.User()
+	user.set('email', req.params.email.toLowerCase())
+	user.set('username', req.params.email.toLowerCase())
+	user.set('password', req.params.password)
+	user.set('fullname', req.params.fullname)
+	user.set('document', req.params.document)
+	user.set('phone', req.params.phone)
+	await user.signUp(null, { useMasterKey: true })
+	return formatUser(user.toJSON())
+  }, {
+	fields: {
+	  email: { required: true },
+	  password: { required: true },
+	  fullname: { required: true },
+	  document: { required: true },
+	  phone: { required: true },
+	}
+  })
+  
