04a855d096c5bd4bca7e907b76ee9a7f0b87f6c6
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-21 12:00:46 -0700

    feat: add main get proffisional

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,8 @@
+require('features/schedule.js')
+
+const Professional = Parse.Object.extend('Professional');
+const Specialty = Parse.Object.extend('Specialty');
+
 Parse.Cloud.define('v1-sign-in', async (req) => {
 	const user = await Parse.User.logIn(req.params.email.toLowerCase(), req.params.password)
 	return formatUser(user.toJSON())
@@ -32,98 +37,31 @@ Parse.Cloud.define('v1-sign-in', async (req) => {
 	}
   })
 
-  Parse.Cloud.define('v1-get-scheduling-slots', async (req) => {
-	const duration = req.params.duration;
-	const professionalId = req.params.professionalId;
-	const startDate = new Date(req.params.startDate);
-	const endDate = new Date(req.params.endDate);
+  Parse.Cloud.define('v1-get-professionals', async (req) => {
+	const query = new Parse.Query(Professional);
+	query.include('specialties', 'insurances', 'services');
 
-	const professional = new Professional();
-	professional.id = professionalId;
-	await professional.fetch({useMasterKey: true});
-
-	const schedulingsQuery = new Parse.Query(Schedule);
-	schedulingsQuery.equalTo('professional', professional);
-	schedulingsQuery.greaterThanOrEqualTo('startDate', startDate);
-	schedulingsQuery.lessThanOrEqualTo('endDate', endDate);
-	schedulingsQuery.ascending('startDate');
-	const schedulings = await schedulingsQuery.find({useMasterKey: true});
-
-	let days = 0;
-	const availableSlots = [];
-
-	while(days < 60) {
-		const currentDate = new Date(startDate.getTime() + days * 24 * 60 * 60 * 1000);
-		days += 1;
-
-		if(currentDate >= endDate) break;
-
-		let weekday = currentDate.getDay();
-		if(weekday == 0) weekday = 7;
-
-		const workSlots = professional.get('scheduleRule').filter((s) => s.weekday == weekday);
-
-		const availableSlotsInDay = [];
-
-		for(const workSlot of workSlots) {
-			const diffStart = new Date(workSlot.startTime) - new Date('2000-01-01T00:00:00.000Z');
-			const diffEnd = new Date(workSlot.endTime) - new Date('2000-01-01T00:00:00.000Z');
-		
-			const workSlotStart = new Date(currentDate.getTime() + diffStart);
-			const workSlotEnd = new Date(currentDate.getTime() + diffEnd);
-
-			let minutes = 0;
-
-			while(minutes < 24 * 60) {
-				const testSlotStart = new Date(workSlotStart.getTime() + minutes * 60 * 1000);
-				const testSlotEnd = new Date(testSlotStart.getTime() + duration * 60 * 1000);
-
-				minutes += professional.get('slotInterval');
-
-				if(testSlotEnd > workSlotEnd) break;
-
-				for(const schedule of schedulings) {
-					if(testSlotEnd <= schedule.get('startDate')) {
-						availableSlotsInDay.push(
-							{
-								startDate: testSlotStart.toISOString(),
-								endDate: testSlotEnd.toISOString()
-							}
-						);
-						break;
-					} else if(testSlotEnd <= schedule.get('endDate') || testSlotStart < schedule.get('endDate')){
-						break;
-					} else if(schedule === schedulings[schedulings.length - 1]) {
-						availableSlotsInDay.push(
-							{
-								startDate: testSlotStart.toISOString(),
-								endDate: testSlotEnd.toISOString()
-							}
-						);
-						break;
-					}
-				}
-			}
-		}
-
-		availableSlots.push(
-			{
-				date: currentDate.toISOString(),
-				slots: availableSlotsInDay,
-			}
-		)
+	if(req.params.specialtyId) {
+		const specialty = new Specialty();
+		specialty.id = req.params.specialtyId;
+		query.equalTo('specialties', specialty);
 	}
 
-	return availableSlots;
+	if(req.params.lat && req.params.long) {
+		const point = new Parse.GeoPoint({latitude: req.params.lat, longitude: req.params.long});
+		query.withinKilometers('location', point, req.params.maxDistance || 50);
+	}
 
+	if(req.params.limit && req.params.skip) {
+		query.limit(req.params.limit);
+		query.skip(req.params.skip);
+	}
+	
+	const results = await query.find({useMasterKey: true});
+	return results.map((r) => formatProfessional(r.toJSON()));
 }, {
 	fields: {
-		duration: {
-			required: true,
-		},
-		professionalId: {
-			required: true
-		}
+		
 	}
 });
 
