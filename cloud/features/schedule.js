d2d0d04d0355396cefec0ee737e8bf42c1382f0b
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-22 11:24:59 -0700

    feat: finally schedule option

--- cloud/features/schedule.js
+++ cloud/features/schedule.js
@@ -1,6 +1,6 @@
 const Professional = Parse.Object.extend('Professional');
 const Schedule = Parse.Object.extend('Schedule');
-const Service = Parse.Object.extend('Service')
+const Service = Parse.Object.extend('Service');
 
 Parse.Cloud.define('v1-get-scheduling-slots', async (req) => {
 	const duration = req.params.duration;
@@ -8,14 +8,110 @@ Parse.Cloud.define('v1-get-scheduling-slots', async (req) => {
 	const startDate = new Date(req.params.startDate);
 	const endDate = new Date(req.params.endDate);
 
+	return getAvailableSlots(duration, professionalId, startDate, endDate);
+
+}, {
+	fields: {
+		duration: {
+			required: true,
+		},
+		professionalId: {
+			required: true
+		}
+	}
+});
+
+Parse.Cloud.define('v1-schedule-services', async (req) => {
+    const professionalId = req.params.professionalId;
+    const serviceIds = req.params.serviceIds;
+    const startDate = new Date(req.params.startDate);
+	const endDate = new Date(req.params.endDate);
+
+    const queryProfessional = new Parse.Query(Professional);
+    queryProfessional.include('services');
+    const professional = await queryProfessional.get(professionalId, {useMasterKey: true});
+
+    const services = professional.get('services').filter((s) => serviceIds.includes(s.id));
+
+    if(services.length != serviceIds.length) throw 'INVALID_SERVICES';
+
+    const duration = services.reduce((partialSum, s) => partialSum + s.get('duration'), 0)
+
+    const availableSlots = await getAvailableSlots(duration, professionalId, startDate, endDate);
+    const isAvailable = availableSlots.some((d) => d.slots.some((s) => s.startDate == startDate.toISOString() && s.endDate == endDate.toISOString()));
+
+    if(!isAvailable) throw 'SLOT_UNAVAILABLE';
+
+    const schedule = new Schedule();
+    schedule.set('startDate', startDate);
+    schedule.set('endDate', endDate);
+    schedule.set('professional', professional);
+    schedule.set('user', req.user);
+    schedule.set('status', 'active');
+    schedule.set('services', services);
+    await schedule.save(null, {useMasterKey: true});
+
+    return schedule;
+}, {
+    requireUser: true,
+});
+
+Parse.Cloud.define('v1-get-user-schedules', async (req) => {
+    const querySchedules = new Parse.Query(Schedule);
+    querySchedules.equalTo('user', req.user);
+    querySchedules.include('services', 'professional', 'professional.specialties');
+    const schedules = await querySchedules.find({useMasterKey: true});
+    return schedules.map((s) => formatSchedule(s.toJSON()));
+}, {
+    requireUser: true,
+});
+
+function formatSchedule(s) {
+    return {
+        id: s.objectId,
+        startDate: s.startDate.iso,
+        endDate: s.endDate.iso,
+        status: s.status,
+        professional: formatProfessional(s.professional),
+        services: s.services.map(formatService)
+    }
+}
+
+function formatService(s) {
+    return {
+        id: s.objectId,
+        name: s.name,
+        price: s.price,
+		duration: s.duration,
+		available: s.available,
+    }
+}
+
+function formatSpecialty(s) {
+	return {
+		id: s.objectId,
+		name: s.name
+	}
+}
+
+function formatProfessional(p) {
+	return {
+		id: p.objectId,
+		name: p.name,
+		specialties: p.specialties.map((s) => formatSpecialty(s)),
+		crm: p.crm,
+	};
+}
+
+async function getAvailableSlots(duration, professionalId, startDate, endDate) {
 	const professional = new Professional();
 	professional.id = professionalId;
 	await professional.fetch({useMasterKey: true});
 
 	const schedulingsQuery = new Parse.Query(Schedule);
 	schedulingsQuery.equalTo('professional', professional);
-	schedulingsQuery.greaterThanOrEqualTo('startDate', startDate);
-	schedulingsQuery.lessThanOrEqualTo('endDate', endDate);
+	schedulingsQuery.greaterThanOrEqualTo('startDate', new Date(startDate.getTime() - 24*60*60*1000));
+	schedulingsQuery.lessThanOrEqualTo('endDate', new Date(endDate.getTime() + 24*60*60*1000));
 	schedulingsQuery.ascending('startDate');
 	const schedulings = await schedulingsQuery.find({useMasterKey: true});
 
@@ -23,7 +119,9 @@ Parse.Cloud.define('v1-get-scheduling-slots', async (req) => {
 	const availableSlots = [];
 
 	while(days < 60) {
-		const currentDate = new Date(startDate.getTime() + days * 24 * 60 * 60 * 1000);
+		let currentDate = new Date(startDate.getTime() + days * 24 * 60 * 60 * 1000);
+        currentDate.setHours(0, 0, 0, 0);
+
 		days += 1;
 
 		if(currentDate >= endDate) break;
@@ -39,8 +137,19 @@ Parse.Cloud.define('v1-get-scheduling-slots', async (req) => {
 			const diffStart = new Date(workSlot.startTime) - new Date('2000-01-01T00:00:00.000Z');
 			const diffEnd = new Date(workSlot.endTime) - new Date('2000-01-01T00:00:00.000Z');
 		
-			const workSlotStart = new Date(currentDate.getTime() + diffStart);
-			const workSlotEnd = new Date(currentDate.getTime() + diffEnd);
+			let workSlotStart = new Date(currentDate.getTime() + diffStart);
+			let workSlotEnd = new Date(currentDate.getTime() + diffEnd);
+
+            if(workSlotStart < startDate && workSlotEnd > startDate) {
+                workSlotStart = startDate;
+            } else if(workSlotStart < startDate && workSlotEnd < startDate) {
+                continue;
+            }
+            if(workSlotStart < endDate && workSlotEnd > endDate) {
+                workSlotEnd = endDate;
+            } else if(workSlotStart > endDate && workSlotEnd > endDate) {
+                continue;
+            }
 
 			let minutes = 0;
 
@@ -85,27 +194,4 @@ Parse.Cloud.define('v1-get-scheduling-slots', async (req) => {
 	}
 
 	return availableSlots;
-
-}, {
-	fields: {
-		duration: {
-			required: true,
-		},
-		professionalId: {
-			required: true
-		}
-	}
-});
-
-Parse.Cloud.define('v1-get-schedule-services', async (req) => {
-    const professionalId = req.params.professionalId
-    const serviceIds = req.params.serviceIds 
-    const queryProfisional = new Parse.Query(Professional)
-    queryProfisional.include('services')
-    const professional = await queryProfisional.get(professionalId, {useMasterKey: true})
-    const services = professional.get('services').filter((s) => serviceIds.includes(s.id))
-    if(services.length != serviceIds.length) throw 'invalid_services'
-    return services
-}, {
-    requireUser: true
-})
+}
