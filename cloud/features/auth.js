376d43e98faf7d5d3c351b1a2b54e3d8dd90f8bd
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-22 11:25:37 -0700

    feat: create file for auth feature

---
+++ cloud/features/auth.js
@@ -0,0 +1,57 @@
+Parse.Cloud.define('v1-sign-in', async (req) => {
+	const user = await Parse.User.logIn(req.params.email.toLowerCase(), req.params.password);
+	return formatUser(user.toJSON());
+}, {
+	fields: {
+		email: {
+			required: true
+		},
+		password: {
+			required: true
+		}
+	}
+});
+
+Parse.Cloud.define('v1-get-user', async (req) => {
+	return formatUser(req.user.toJSON());
+});
+
+Parse.Cloud.define('v1-sign-up', async (req) => {
+	const user = new Parse.User();
+	user.set('email', req.params.email.toLowerCase());
+	user.set('username', req.params.email.toLowerCase());
+	user.set('fullname', req.params.fullname);
+	user.set('phone', req.params.phone);
+	user.set('document', req.params.document);
+	user.set('password', req.params.password);
+	await user.signUp(null, {useMasterKey: true});
+	return formatUser(user.toJSON());
+}, {
+	fields: {
+		email: {
+			required: true
+		},
+		password: {
+			required: true
+		},
+		fullname: {
+			required: true
+		},
+		document: {
+			required: true
+		},
+		phone: {
+			required: true
+		},
+	}
+});
+
+function formatUser(u) {
+	return {
+		id: u.objectId,
+		token: u.sessionToken,
+		fullname: u.fullname,
+		document: u.document,
+		phone: u.phone,
+	}
+}
