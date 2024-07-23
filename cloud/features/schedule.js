f9af9d0c2c629f0056fc20cc0febfd540fd4d31c
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-23 08:30:22 -0700

    refactor: remove line blank

--- cloud/features/schedule.js
+++ cloud/features/schedule.js
@@ -9,7 +9,6 @@ Parse.Cloud.define('v1-get-scheduling-slots', async (req) => {
 	const professionalId = req.params.professionalId;
 	const startDate = new Date(req.params.startDate);
 	const endDate = new Date(req.params.endDate);
-
 	return getAvailableSlots(duration, professionalId, startDate, endDate);
 
 }, {
@@ -28,22 +27,15 @@ Parse.Cloud.define('v1-schedule-services', async (req) => {
     const serviceIds = req.params.serviceIds;
     const startDate = new Date(req.params.startDate);
 	const endDate = new Date(req.params.endDate);
-
     const queryProfessional = new Parse.Query(Professional);
     queryProfessional.include('services');
     const professional = await queryProfessional.get(professionalId, {useMasterKey: true});
-
     const services = professional.get('services').filter((s) => serviceIds.includes(s.id));
-
     if(services.length != serviceIds.length) throw 'INVALID_SERVICES';
-
     const duration = services.reduce((partialSum, s) => partialSum + s.get('duration'), 0)
-
     const availableSlots = await getAvailableSlots(duration, professionalId, startDate, endDate);
     const isAvailable = availableSlots.some((d) => d.slots.some((s) => s.startDate == startDate.toISOString() && s.endDate == endDate.toISOString()));
-
     if(!isAvailable) throw 'SLOT_UNAVAILABLE';
-
     const schedule = new Schedule();
     schedule.set('startDate', startDate);
     schedule.set('endDate', endDate);
@@ -52,7 +44,6 @@ Parse.Cloud.define('v1-schedule-services', async (req) => {
     schedule.set('status', 'active');
     schedule.set('services', services);
     await schedule.save(null, {useMasterKey: true});
-
     return schedule;
 }, {
     requireUser: true,
@@ -72,9 +63,7 @@ Parse.Cloud.define('v1-cancel-schedule', async (req) => {
 	const schedule = new Schedule();
 	schedule.id = req.params.scheduleId;
 	await schedule.fetch({useMasterKey: true});
-
 	if(req.user.id != schedule.get('user').id) throw 'INVALID_USER';
-
 	schedule.set('status', 'canceled');
 	await schedule.save(null, {useMasterKey: true});
 }, {
@@ -90,9 +79,7 @@ Parse.Cloud.define('v1-professional-cancel-schedule', async (req) => {
 	const querySchedule = new Parse.Query(Schedule);
 	querySchedule.include('professional');
 	const schedule = await querySchedule.get(req.params.scheduleId, {useMasterKey: true});
-	
 	if(req.user.id != schedule.get('professional').get('owner').id && !schedule.get('professional').get('users').some((u) => u.id == req.user.id)) throw 'INVALID_USER';
-
 	schedule.set('status', 'canceled');
 	await schedule.save(null, {useMasterKey: true});
 }, {
@@ -108,9 +95,7 @@ Parse.Cloud.define('v1-get-professional-agenda', async (req) => {
 	const queryProfessional = new Parse.Query(Professional);
     queryProfessional.equalTo('owner', req.user);
     const professional = await queryProfessional.first({useMasterKey: true});
-
     if(!professional) throw 'INVALID_PROFESSIONAL';
-
 	const querySchedules = new Parse.Query(Schedule);
 	querySchedules.equalTo('professional', professional);
 	querySchedules.greaterThanOrEqualTo('startDate', new Date(req.params.startDate));
@@ -119,9 +104,7 @@ Parse.Cloud.define('v1-get-professional-agenda', async (req) => {
 	querySchedules.ascending('startDate');
 	querySchedules.include('user', 'services');
 	querySchedules.exclude('professional');
-	
 	const schedules = await querySchedules.find({useMasterKey: true});
-
 	return schedules.map((s) => formatSchedule(s.toJSON()));
 }, {
 	requireUser: true,
@@ -139,39 +122,28 @@ async function getAvailableSlots(duration, professionalId, startDate, endDate) {
 	const professional = new Professional();
 	professional.id = professionalId;
 	await professional.fetch({useMasterKey: true});
-
 	const schedulingsQuery = new Parse.Query(Schedule);
 	schedulingsQuery.equalTo('professional', professional);
 	schedulingsQuery.greaterThanOrEqualTo('startDate', new Date(startDate.getTime() - 24*60*60*1000));
 	schedulingsQuery.lessThanOrEqualTo('endDate', new Date(endDate.getTime() + 24*60*60*1000));
 	schedulingsQuery.ascending('startDate');
 	const schedulings = await schedulingsQuery.find({useMasterKey: true});
-
 	let days = 0;
 	const availableSlots = [];
-
 	while(days < 60) {
 		let currentDate = new Date(startDate.getTime() + days * 24 * 60 * 60 * 1000);
         currentDate.setHours(0, 0, 0, 0);
-
 		days += 1;
-
 		if(currentDate >= endDate) break;
-
 		let weekday = currentDate.getDay();
 		if(weekday == 0) weekday = 7;
-
 		const workSlots = professional.get('scheduleRule').filter((s) => s.weekday == weekday);
-
 		const availableSlotsInDay = [];
-
 		for(const workSlot of workSlots) {
 			const diffStart = new Date(workSlot.startTime) - new Date('2000-01-01T00:00:00.000Z');
 			const diffEnd = new Date(workSlot.endTime) - new Date('2000-01-01T00:00:00.000Z');
-		
 			let workSlotStart = new Date(currentDate.getTime() + diffStart);
 			let workSlotEnd = new Date(currentDate.getTime() + diffEnd);
-
             if(workSlotStart < startDate && workSlotEnd > startDate) {
                 workSlotStart = startDate;
             } else if(workSlotStart < startDate && workSlotEnd < startDate) {
@@ -182,17 +154,12 @@ async function getAvailableSlots(duration, professionalId, startDate, endDate) {
             } else if(workSlotStart > endDate && workSlotEnd > endDate) {
                 continue;
             }
-
 			let minutes = 0;
-
 			while(minutes < 24 * 60) {
 				const testSlotStart = new Date(workSlotStart.getTime() + minutes * 60 * 1000);
 				const testSlotEnd = new Date(testSlotStart.getTime() + duration * 60 * 1000);
-
 				minutes += professional.get('slotInterval');
-
 				if(testSlotEnd > workSlotEnd) break;
-
 				for(const schedule of schedulings) {
 					if(testSlotEnd <= schedule.get('startDate')) {
 						availableSlotsInDay.push(
@@ -216,7 +183,6 @@ async function getAvailableSlots(duration, professionalId, startDate, endDate) {
 				}
 			}
 		}
-
 		availableSlots.push(
 			{
 				date: currentDate.toISOString(),
@@ -224,6 +190,5 @@ async function getAvailableSlots(duration, professionalId, startDate, endDate) {
 			}
 		)
 	}
-
 	return availableSlots;
 }
